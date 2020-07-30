6106235eee77f115f86a2481eed211dee9e42979
Brayan Bernal <brayanbernal0710@gmail.com>
2020-07-30 17:51:43 -0400

    Agregando cambios y limpiando otro poco.

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,25 @@
+<?php namespace Cinema\Http\Controllers;
+
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller {
+
+	public function index()
+	{
+		return view('index');
+	}
+
+	public function contacto()
+	{
+		return view('contacto');
+	}
+
+	public function reviews()
+	{
+		return view('reviews');
+	}
+
+}
