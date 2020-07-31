43188a0e8732de9423039f99b7a2dc77db1ac912
Brayan Bernal <brayanbernal0710@gmail.com>
2020-07-31 15:53:27 -0400

    Agregando deleteSoft para no borrar los datos de la base de datos permanentemente, limpiando comentarios innecesarios, agregando campo deleted_at con un nuevo migrate a la tabla users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,32 +14,17 @@ use \Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
 		$users = User::paginate(10);
 		return view('user.index', compact('users'));
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
 	public function create()
 	{
 		return view('user.create');
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
 	public function store(UserCreateRequest $request)
 	{
 		User::create([
@@ -50,35 +35,17 @@ class UserController extends Controller
 		return redirect('/usuario')->with('message', 'Usuario Registrado Exitosamente.');
 	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function show($id)
 	{
 		//
 	}
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function edit($id)
 	{
 		$user = User::find($id);
 		return view('user.edit', ['user' => $user]);
 	}
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function update($id, UserUpdateRequest $request)
 	{
 		$user = User::find($id);
@@ -89,15 +56,11 @@ class UserController extends Controller
 		return Redirect::to('/usuario');
 	}
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
 	public function destroy($id)
 	{
-		User::destroy($id);
+		// User::destroy($id); // Destruye el elemento eliminando permanentemente el registro.
+		$user = User::find($id);
+		$user->delete(); //Oculta el elemento seteando el campo deleted_at en la base de datos.
 		Session::flash('message', 'Usuario Eliminado Exitosamente');
 		return Redirect::to('/usuario');
 	}
--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace Cinema;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
@@ -12,28 +13,15 @@ use Illuminate\Support\Facades\Hash;
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
-	use Authenticatable, CanResetPassword;
+	use Authenticatable, CanResetPassword, SoftDeletes;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
 	protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
 	protected $fillable = ['name', 'email', 'password'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
 	protected $hidden = ['password', 'remember_token'];
+
+	protected $dates = ['deleted_at'];
 
 	public function setPasswordAttribute($value)
 	{
